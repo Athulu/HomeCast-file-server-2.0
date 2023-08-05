d3a353a0323809bd1a628f6a94877c3fe2f089ac
Athulu <nikodem1703@gmail.com>
2023-08-05 14:09:15 +0200

    zapisywanie h2 do pliku przy zamknięciu

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE video ALTER COLUMN subtitle VARCHAR(4096);
-
-INSERT INTO sources(mime, type, url)
-VALUES ('videos/mp4', 'mp4', 's02e02.Jujutsu Kaisen.mp4'),
-       ('videos/mp4', 'mp4', 's02e01.Jujutsu Kaisen.mp4');
-
-INSERT INTO video(file_name, episode, title, subtitle, thumb, image480x270, image780x1200, duration, sourceID)
-VALUES ('aaaaaa', 's02e02', 'Jujutsu Kaisen','subtitle1', 's02e02.Jujutsu Kaisen480x270.png', 's02e02.Jujutsu Kaisen480x270.png', 'bbb.png', 100, 1),
-       ('bbbbbb', 's02e01', 'Jujutsu Kaisen','subtitle2', 's02e01.Jujutsu Kaisen480x270.png', 's02e01.Jujutsu Kaisen480x270.png', 'bbb.png', 100, 2);
