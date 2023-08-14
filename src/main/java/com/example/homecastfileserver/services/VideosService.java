21bcd209b282a8a91baec2d51b7f874e649ca7ed
Athulu <nikodem1703@gmail.com>
2023-08-14 22:09:59 +0200

    dodanie sortowania po nazwach seriali, a pozniej odpowiednio po odcinkach

--- src/main/java/com/example/homecastfileserver/services/VideosService.java
+++ src/main/java/com/example/homecastfileserver/services/VideosService.java
@@ -3,6 +3,7 @@ package com.example.homecastfileserver.services;
 import com.example.homecastfileserver.configs.HomeCastConfig;
 import com.example.homecastfileserver.dao.Video;
 import com.example.homecastfileserver.dto.UltimateDTO;
+import com.example.homecastfileserver.dto.VideoDTO;
 import com.example.homecastfileserver.mappers.VideosMapper;
 import com.example.homecastfileserver.repositories.VideosRepository;
 import jakarta.persistence.EntityManager;
@@ -10,8 +11,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,13 +21,59 @@ public class VideosService {
     private final VideosRepository videosRepository;
     private final VideosMapper videosMapper;
     private final HomeCastConfig homeCastConfig;
+    private static final int CYBER_PAPAJ_2137 = 2137;
 
-    public UltimateDTO getListOfVideos(){
-        return UltimateDTO.create(homeCastConfig.getIp() + "/mp4/",homeCastConfig.getIp() + "/images/", videosMapper.videoListToVideoDTOList(videosRepository.findAllBy()));
+    public UltimateDTO getListOfVideosSortedByFileName(){
+        return UltimateDTO.create(homeCastConfig.getIp() + "/mp4/",homeCastConfig.getIp() + "/images/", videosMapper.videoListToVideoDTOList(videosRepository.findAllBy().stream().sorted(Comparator.comparing((Video::getFileName))).collect(Collectors.toList())));
+    }
+
+    public UltimateDTO getListOfVideosSortedBySeriesAndEpisode(){
+        List<VideoDTO> videoDTOList = videosMapper.videoListToVideoDTOList(videosRepository.findAllBy());
+        List<VideoDTO> sortedList = new ArrayList<>();
+        Map<String, List<VideoDTO>> listHashMap = new HashMap<>();
+
+        for (VideoDTO videoDTO : videoDTOList) {
+            if (!listHashMap.containsKey(videoDTO.getTitle())) {
+                listHashMap.put(videoDTO.getTitle(), new ArrayList<>());
+            }
+            listHashMap.get(videoDTO.getTitle()).add(videoDTO);
+        }
+
+        for (String title: listHashMap.keySet()) {
+            List<VideoDTO> lista = listHashMap.get(title).stream().sorted(Comparator.comparing((vid) -> {
+                //format: Odcinek XX
+                String[] fullEpisodeString = vid.getEpisode().split(" ");
+                if (fullEpisodeString.length >= 2) {
+                    try {
+                        String episodeNumber = fullEpisodeString[1];
+                        return Integer.parseInt(episodeNumber);
+                    } catch (NumberFormatException e) {
+                        return CYBER_PAPAJ_2137;
+                    }
+
+                }
+                return CYBER_PAPAJ_2137;
+            })).toList();
+            sortedList.addAll(lista);
+        }
+
+        return UltimateDTO.create(homeCastConfig.getIp() + "/mp4/",homeCastConfig.getIp() + "/images/", sortedList);
     }
 
     public List<String> getVideosFileNames(){
         return videosRepository.findAllBy().stream().map(Video::getFileName).collect(Collectors.toList());
+    }
+
+    private static int extractEpisodeNumber(String episode) {
+        String[] parts = episode.split(" ");
+        if (parts.length >= 2) {
+            try {
+                return Integer.parseInt(parts[1]);
+            } catch (NumberFormatException e) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        return Integer.MAX_VALUE;
     }
 
     public Video getVideoByFileName(String fileName){
